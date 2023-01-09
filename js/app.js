2fbd2cbeaed274f628765c59ba58fcca9dc552f3
Your Name <you@example.com>
2023-01-09 17:05:11 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,98 @@
+
+
+
+// let a = 4
+// let b = 5
+//  a = a + b
+//  b = a - b
+//  a = a - b
+//  console.log( a )
+
+      
+// let speed = 1200
+
+
+// if ( speed <= 70){
+//     console.log( "Hammasi yaxshi");
+// }
+// else if  ( speed > 70 && speed % 5 ===0){
+  
+//     console.log( " 1 point")
+//     speed = point
+// }
+
+// else if ( point > 12){
+//     console.log("Guvohnoma olindi");
+// }
+// else if ( point < 12){
+//     console.log(point);
+// }
+
+
+
+
+
+
+// let random = Math.random() * 50
+//     random = parseInt(random)
+
+     
+    //  if ( random % 5 ===0 && random % 3 ===0){
+    //     console.log( random + " bu son 3 ga va 5 ga bo`linadi")
+    // }
+    // else if ( random % 5 ===0){
+    //     console.log( random + " bu son 5 ga bo`linadi")
+    // }
+    // else if ( random % 3 ===0){
+    //     console.log( random + " bu son 3ga bo`linadi")
+    // }
+    // else{
+    //     console.log( random + " 3 ga ham 5 ga ham bo`linmaydi")
+    // }
+
+
+
+    // if(random % 2 ===0){
+    //     console.log( random +  " soni juft son" , );
+    // }
+    // else {
+    //     console.log( random +  " soni toq son" , );
+    // }
+
+
+
+    //  random % 2 ===0 ?
+    //  console.log( random + " juft son") : 
+    //  console.log( random + " toq son");
+
+
+
+    //   random % 2 ===0 ? console.log( random + " juft son")
+    // : random % 2 !=0 ?  console.log( random + " toq son")
+    // :                   console.log( random + "0 ga teng")
+
+
+
+
+
+    let a = 144
+    let b = 133
+
+
+    let count = 5-a
+    let count1 = 5 - b
+     count = Math.abs(count)
+     count1 = Math.abs(count1)
+
+if (count > count1){
+    console.log(b);
+}
+else if ( count === count1){
+    console.log(a , b);
+}
+else{
+    console.log(a);
+}
+
+ 
+
